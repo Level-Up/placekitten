01ce599fd372b2f1c3d889e08637e4e923fc2442
Aaron Bassett <aaron@thisislevelup.com>
2011-06-10 23:45:01 +0100

    Create template tag

---
+++ templatetags/placekitten.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from classytags.core import Tag, Options
+from classytags.arguments import Argument
+from django import template
+
+
+register = template.Library()
+
+
+class placekitten(Tag):
+    """
+    Show a place-holder image from 
+    placekitten.com
+    
+    Usage:
+    {% placekitten 200 350 %}
+    {% placekitten 200 350 "b&w" %}
+    
+    """
+
+    options = Options(
+        Argument('width', required=True),
+        Argument('height', required=True),
+        Argument('color', required=False),
+    )
+
+    def render_tag(self, context, width, height, color):
+        host = "http://placekitten.com/g/" if color else "http://placekitten.com/"
+        url = "%s%d/%d" % (host, width, height)
+                
+        return '<img src="%s" alt="I can haz placeholder?" />' % url
+
+register.tag(placekitten)
